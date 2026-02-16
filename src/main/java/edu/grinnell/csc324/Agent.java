9e0890874068bf7be1c0dfbdbb75434913d8cdf4
maral <erdenemaral@grinnell.edu>
2026-02-16 15:50:46 -0600

    adding agent

--- src/main/java/edu/grinnell/csc324/Agent.java
+++ src/main/java/edu/grinnell/csc324/Agent.java
@@ -2,44 +2,22 @@ package edu.grinnell.csc324;
 import java.util.Random;
 
 public class Agent {
-    int[] moveProbability;
-    public Position curPosition;
-    public int foodEaten = 0;
-    public int stepsTaken = 0;
-    public boolean inFinalState = false;
     Random moveGenerator;
+    // 0 -> left 1-> right 2-> up 3-> down 4-> still
+    int currentMove;
+    public int weight;
+    public int payOff;
+    // -1 -> penalty, 0 -> non-penalty, 1 -> terminal
+    public int Type;
 
-    public Agent(int left, int up, int right, int down, int startRow, int startColumn) {
-        this.moveProbability = new int[] {left, up+left, right+up+left, down+right+up+left};
-        this.curPosition = new Position(startRow, startColumn);
+
+    public Agent(int weight, int Type, int payOff) {
         this.moveGenerator = new Random();
+        this.payOff = payOff;
     }
 
-    public Position tryMove() {
-            int moveChoice = this.moveGenerator.nextInt(100) + 1;
-            if(moveChoice < this.moveProbability[0]) {
-                return new Position(curPosition.row, curPosition.column - 1);
-            } else if(moveChoice < this.moveProbability[1]) {
-                return new Position(curPosition.row - 1, curPosition.column);
-            } else if(moveChoice < this.moveProbability[2]) {
-                return new Position(curPosition.row, curPosition.column + 1);
-            } else {
-                return new Position(curPosition.row + 1, curPosition.column);
-            }
-        
-    }
-
-    public void eatFood() {
-        this.foodEaten += 1;
-    }
-
-    public class Position {
-        public int row;
-        public int column;
-
-        public Position(int row, int column) {
-            this.row = row;
-            this.column = column;
-        }
+    public int voteMove() {
+        currentMove = this.moveGenerator.nextInt(5);
+        return currentMove;
     }
 }
