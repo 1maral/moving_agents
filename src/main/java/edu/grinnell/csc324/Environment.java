c5cae0c0f2d1c2e4f25cf01fdfee72d72d46cb1a
Maral Bat-Erdene <maralb@Marals-MacBook-Pro.local>
2026-02-17 16:58:51 -0600

    adding functions

--- src/main/java/edu/grinnell/csc324/Environment.java
+++ src/main/java/edu/grinnell/csc324/Environment.java
@@ -1,23 +1,130 @@
 package edu.grinnell.csc324;
 
 public class Environment {
-  private char[][] board;
+  private Agent[][] board;
+  int[] objSize;
+  int[] objTopLeft;
+  int decidedMove;
 
   public Environment() {
-    board = new char[25][25];
-    // initialize();
+    board = new Agent[7][7];
+    objSize = new int[2];
+    objTopLeft = new int[2];
+    initialize();
+  }
+
+  private void initialize() {
+    for (int i = 0; i < 7; i++) {
+      board[i][0].Type = -1;
+      board[i][6].Type = -1;
+
+      for (int j = 1; j < 6; j++) {
+        board[i][j].Type = 0;
+      }
+    } 
+    board[5][2].Type = 1;
+    objSize[0] = 2;
+    objSize[1] = 3;
+
+    objTopLeft[0] = 0;
+    objTopLeft[1] = 2;
+  }
+
+  public void updateWeight(){
+    for (int i = 0; i < 7; i++) {
+      for (int j = 0; j < 7; j++) {
+        if (i == objTopLeft[0] || i == (objTopLeft[0] + 1)) {
+          if (j == objTopLeft[1] || j == (objTopLeft[1] + 1) || j == (objTopLeft[1] + 2)) {
+            board[i][j].weight = 5;
+          } else if (j == (objTopLeft[1] - 1) || j == (objTopLeft[1] + 3)){
+            board[i][j].weight = 1;
+          }
+        } else if (i == objTopLeft[0] - 1 || i == (objTopLeft[0] + 2)) {
+          if (j == objTopLeft[1] || j == (objTopLeft[1] + 1) || j == (objTopLeft[1] + 2)) {
+            board[i][j].weight = 1;
+          }
+        } else {
+          board[i][j].weight = 0;
+        }
+      }
+    }
+  }
+
+  public void decideMove() {
+    int[] moves = new int[5];
+    for (int i = 0; i < 7; i++) {
+      for (int j = 0; j < 7; j++) {
+        moves[board[i][j].voteMove()] = moves[board[i][j].voteMove()] + board[i][j].weight;
+      }
+    }
+    int max = -1;
+    for (int i = 0; i < 5; i++) {
+      if(moves[i] > max) {
+        max = i;
+      }
+    }
+    decidedMove = max;
+  }
+
+  public void moveObj() {
+    decideMove();
+    if (decidedMove == 0) {
+      if (objTopLeft[1] != 0) {
+        objTopLeft[1]--;
+      }
+    } else if (decidedMove == 1) {
+      if (objTopLeft[1] != 4) {
+        objTopLeft[1]++;
+      }
+    } else if (decidedMove == 2) {
+      if (objTopLeft[0] != 0) {
+        objTopLeft[0]--;
+      }
+    } else if (decidedMove == 3) {
+      if (objTopLeft[0] != 5) {
+        objTopLeft[0]++;
+      }
+    } else {
+      // no move
+    }
+    checkType();
+  }
+
+  public boolean checkType() {
+    if (board[objTopLeft[0]][objTopLeft[1]].Type == -1 || board[objTopLeft[0]][objTopLeft[1] + 2].Type == -1) {
+        assignPayoffs(-10);
+        return true;
+    } else if (board[objTopLeft[0]][objTopLeft[1]].Type == 1) {
+      assignPayoffs(100);
+      return false;
+    } else {
+      assignPayoffs(-1);
+      return true;
+    }
+  }
+
+  public void assignPayoffs(int score) {
+    for (int i = 0; i < 7; i++) {
+      for (int j = 0; j < 7; j++) {
+        board[i][j].payOff += score;
+      }
+    }
   }
 
   public void print() {
-    for (int row = 0; row < 25; row++) {
-      for (int column = 0; column < 25; column++) {
-        System.out.print(board[row][column] + " ");
+    for (int row = 0; row < 7; row++) {
+      for (int column = 0; column < 7; column++) {
+        System.out.print(board[row][column].weight + " ");
       }
       System.out.println();
     }
   }
   public static void main(String[] args) {
-    // runTest();
+    Environment environment = new Environment();
+    while (environment.checkType()) {
+      environment.moveObj();
+      environment.print();
+    }
   }
 }
 
